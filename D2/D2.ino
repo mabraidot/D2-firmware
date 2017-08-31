db3fe7ec05ec7fce174f0a1a5419a923380e75ba
mabraidot <mabraidot@gmail.com>
2017-08-30 21:14:08 -0300

    Ajuste de relación de poleas y coreografía de prueba

--- D2/D2.ino
+++ D2/D2.ino
@@ -12,9 +12,9 @@ AccelStepper stepperA(AccelStepper::DRIVER, X_STEP_PIN, X_DIR_PIN);
 AccelStepper stepperB(AccelStepper::DRIVER, Y_STEP_PIN, Y_DIR_PIN);
 AccelStepper stepperC(AccelStepper::DRIVER, Z_STEP_PIN, Z_DIR_PIN);
 */
-A4988 stepperA(400, X_DIR_PIN, X_STEP_PIN, X_ENABLE_PIN);
-A4988 stepperB(400, Y_DIR_PIN, Y_STEP_PIN, Y_ENABLE_PIN);
-A4988 stepperC(400, Z_DIR_PIN, Z_STEP_PIN, Z_ENABLE_PIN);
+A4988 stepperA(400*6.2, X_DIR_PIN, X_STEP_PIN, X_ENABLE_PIN);
+A4988 stepperB(400*6.2, Y_DIR_PIN, Y_STEP_PIN, Y_ENABLE_PIN);
+A4988 stepperC(400*6.2, Z_DIR_PIN, Z_STEP_PIN, Z_ENABLE_PIN);
 SyncDriver motors(stepperA, stepperB, stepperC);
 
 void setup() {
@@ -39,7 +39,7 @@ void setup() {
   stepperC.setAcceleration(100.0);
   */
   
-  char rpm = 150;
+  char rpm = 1400;
   stepperA.begin(rpm,2);
   stepperA.setSpeedProfile(LINEAR_SPEED, 3000, 2000);
   stepperB.begin(rpm,2);
@@ -65,21 +65,43 @@ void stepper_rotate(int deg){
   stepperB.moveTo(deg);
   stepperC.moveTo(deg);
   */
-  
-  if(deg > 0 && deg > 75){deg = 75;}
-  if(deg < 0 && deg < -75){deg = -75;}
+  if(deg > 0 && deg > 90){deg = 90;}
+  if(deg < 0 && deg < -90){deg = -90;}
   motors.rotate(deg, deg, deg);
 }
-void stepper_choreography(){
-  motors.rotate(-40, -40, -40);
-  motors.rotate(30, 30, 40);
-  motors.rotate(-30, -30, -40);
-  motors.rotate(30, 40, 30);
-  motors.rotate(-30, -40, -30);
-  motors.rotate(40, 30, 30);
-  motors.rotate(-40, -30, -30);
+void stepper_choreography(int mode = 0){
+  if(mode == 0){
+    motors.rotate(-20, -20, -20);
+    motors.rotate(0, 0, 40);
+    motors.rotate(40, 0, -40);
+    motors.rotate(-40, 40, 0);
+    motors.rotate(0, -40, 40);
+    motors.rotate(0, 0, -40);
+  
+    motors.rotate(-40, -40, -40);
+    motors.rotate(0, 0, 20);
+    motors.rotate(20, 0, -20);
+    motors.rotate(-20, 20, 0);
+    motors.rotate(0, -20, 20);
+    motors.rotate(0, 0, -20);
+  
+    motors.rotate(60, 60, 60);
+  }
+  
+  if(mode == 1){
+    motors.rotate(-30, -30, -30);
+    
+    motors.rotate(10, 10, 30);
+    motors.rotate(-10, -10, -30);
 
-  motors.rotate(40, 40, 40);
+    motors.rotate(10, 30, 10);
+    motors.rotate(-10, -30, -10);
+
+    motors.rotate(30, 10, 10);
+    motors.rotate(-30, -10, -10);
+
+    motors.rotate(30, 30, 30);
+  }
 }
 
 
@@ -90,7 +112,8 @@ void process_serial(){
     case 'H': help(); break;
     case 'F': stepper_rotate(Serial.parseInt()); break;
     case 'B': stepper_rotate(-1*Serial.parseInt()); break;
-    case 'C': stepper_choreography(); break;
+    case 'C': stepper_choreography(0); break;
+    case 'P': stepper_choreography(1); break;
   }
   
   while (Serial.read() != 10); // dump extra characters till LF is seen (you can use CRLF or just LF)
@@ -105,4 +128,6 @@ void help() {
   Serial.println(F("F123: sets the target rotation for the motor to 123 degrees, Forward"));
   Serial.println(F("B123: sets the target rotation for the motor to 123 degrees, Backward"));
   Serial.println(F("C: Little choreography"));
+  Serial.println(F("P: P&P choreography"));
+  
 }
